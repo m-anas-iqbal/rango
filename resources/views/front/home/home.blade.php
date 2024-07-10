14502f2ea9ca7a9815357a4bbef262b84d9c4f6d
Sameer <sameersiddiquiofficial@gmail.com>
2024-07-11 02:47:19 +0500

    homepage

---
+++ resources/views/front/home/home.blade.php
@@ -0,0 +1,119 @@
+<div class="hero-section">
+
+</div>
+<div class="container cate_card my-5">
+    <div class="row">
+        <div class="col-md-6 p-4">
+            <div class="card p-5 r-bg-red rounded-20px">
+                <h1 class="fw-bold py-3 text-white">Wooden Toys</h1>
+                <img src="{{ asset('frontend/assets/images/toys/wooden.png') }}"
+                    class="position-absolute translate-middle-y" alt="">
+            </div>
+        </div>
+        <div class="col-md-6 p-4">
+            <div class="card p-5 r-bg-blue rounded-20px">
+                <h1 class="fw-bold py-3 text-white">Stuffed Animals</h1>
+                <img src="{{ asset('frontend/assets/images/toys/bear.png') }}"
+                    class="position-absolute translate-middle-y" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container home_products my-5">
+    <div class="d-flex justify-content-between">
+        <h3 class="fw-bold">Stuffed Animals</h3>
+        <a href="" class="fw-bold">See All <i class="fa-solid fa-arrow-right"></i></a>
+    </div>
+    <hr class="start-border">
+    <div class="product-items">
+        <div class="row">
+            <div class="col-md-3 p-3">
+                <div class="card text-center p-3 shadow rounded-10px border-0">
+                    <img src="{{ asset('frontend/assets/images/toys/bear.png') }}" class="img-fluid" alt="">
+                    <h4 class="w-fit mx-auto">Teddy Bear</h4>
+                    <div class="price-label w-fit small r-bg-green rounded-pill mx-auto py-1 px-2 text-white">$24 USD
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-3">
+                <div class="card text-center p-3 shadow rounded-10px border-0">
+                    <img src="{{ asset('frontend/assets/images/toys/bear.png') }}" class="img-fluid" alt="">
+                    <h4 class="w-fit mx-auto">Teddy Bear</h4>
+                    <div class="price-label w-fit small r-bg-green rounded-pill mx-auto py-1 px-2 text-white">$24 USD
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-3">
+                <div class="card text-center p-3 shadow rounded-10px border-0">
+                    <img src="{{ asset('frontend/assets/images/toys/bear.png') }}" class="img-fluid" alt="">
+                    <h4 class="w-fit mx-auto">Teddy Bear</h4>
+                    <div class="price-label w-fit small r-bg-green rounded-pill mx-auto py-1 px-2 text-white">$24 USD
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-3">
+                <div class="card text-center p-3 shadow rounded-10px border-0">
+                    <img src="{{ asset('frontend/assets/images/toys/bear.png') }}" class="img-fluid" alt="">
+                    <h4 class="w-fit mx-auto">Teddy Bear</h4>
+                    <div class="price-label w-fit small r-bg-green rounded-pill mx-auto py-1 px-2 text-white">$24 USD
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container home_products my-5">
+    <div class="d-flex justify-content-between">
+        <h3 class="fw-bold">Wooden Toys</h3>
+        <a href="" class="fw-bold">See All <i class="fa-solid fa-arrow-right"></i></a>
+    </div>
+    <hr class="start-border">
+    <div class="product-items">
+        <div class="row">
+            <div class="col-md-3 p-3">
+                <div class="card text-center p-3 shadow rounded-10px border-0">
+                    <img src="{{ asset('frontend/assets/images/toys/wooden.png') }}" class="img-fluid" alt="">
+                    <h4 class="w-fit mx-auto">Wooden toy</h4>
+                    <div class="price-label w-fit small r-bg-green rounded-pill mx-auto py-1 px-2 text-white">$24 USD
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-3">
+                <div class="card text-center p-3 shadow rounded-10px border-0">
+                    <img src="{{ asset('frontend/assets/images/toys/wooden.png') }}" class="img-fluid" alt="">
+                    <h4 class="w-fit mx-auto">Wooden toy</h4>
+                    <div class="price-label w-fit small r-bg-green rounded-pill mx-auto py-1 px-2 text-white">$24 USD
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-3">
+                <div class="card text-center p-3 shadow rounded-10px border-0">
+                    <img src="{{ asset('frontend/assets/images/toys/wooden.png') }}" class="img-fluid" alt="">
+                    <h4 class="w-fit mx-auto">Wooden toy</h4>
+                    <div class="price-label w-fit small r-bg-green rounded-pill mx-auto py-1 px-2 text-white">$24 USD
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 p-3">
+                <div class="card text-center p-3 shadow rounded-10px border-0">
+                    <img src="{{ asset('frontend/assets/images/toys/wooden.png') }}" class="img-fluid" alt="">
+                    <h4 class="w-fit mx-auto">Wooden toy</h4>
+                    <div class="price-label w-fit small r-bg-green rounded-pill mx-auto py-1 px-2 text-white">$24 USD
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="parallax d-grid place-content-center"
+    style="--bg-image: url('https://as1.ftcdn.net/v2/jpg/04/76/79/70/1000_F_476797090_GD9J6JoeR956o2yXxMBEqQEw0xnlgAx5.jpg');">
+    <div class="container p-5 text-center">
+        <h2>Blah bla bla</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quam autem excepturi suscipit eum nisi,
+            nihil voluptatem, natus laudantium aspernatur possimus. Numquam cum dolor illum.</p>
+        <button>Read More</button>
+    </div>
+</div>
