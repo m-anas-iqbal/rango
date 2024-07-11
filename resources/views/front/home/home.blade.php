7182a3669780e0da14a8a99acc1302e637c10aa4
sam <sameer.siddiqui@ossols.com>
2024-07-11 16:35:53 +0500

    hero section

--- resources/views/front/home/home.blade.php
+++ resources/views/front/home/home.blade.php
@@ -1,5 +1,20 @@
-<div class="hero-section">
-
+<div class="hero-section overflow-hidden">
+    <div class="top"><h2><span class="r-text-blue">R</span><span class="r-text-green">A</span><span>N</span><span class="r-text-red">G</span><span class="r-text-yellow">O</span></h2></div>
+    <div class="center h-100 container">
+        <div class="row h-100 align-items-center">
+            <div class="col-md-6 text-white content">
+                <h5 class="fw-bold small">Your One-Stop Shop for Toys!</h5>
+                <h1 class="fw-bold">RANGO CART</h1>
+                <p class="small">Discover a world of fun and adventure with our wide range of toys for all ages! From educational toys to the latest action figures, we've got something special for every child. Shop now and bring joy home!</p>
+                <a href="" class="btn btn-outline-light">New Arrivals</a>
+                <a href="" class="btn btn-outline-light">Shop Now</a>
+            </div>
+            <div class="col-md-6">
+                <img src="{{asset('frontend/assets/images/hero.svg')}}" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="bottom"><h2><span class="r-text-blue">R</span><span class="r-text-green">A</span><span>N</span><span class="r-text-red">G</span><span class="r-text-yellow">O</span></h2></div>
 </div>
 <div class="container cate_card my-5">
     <div class="row">
@@ -109,7 +124,7 @@
 </div>
 
 <div class="parallax d-grid place-content-center"
-    style="--bg-image: url('https://as1.ftcdn.net/v2/jpg/04/76/79/70/1000_F_476797090_GD9J6JoeR956o2yXxMBEqQEw0xnlgAx5.jpg');">
+    style="--bg-image: url('https://t3.ftcdn.net/jpg/03/23/24/82/360_F_323248211_HNMfkovpijljYs15WasG0XPCTYY2TKrb.jpg');">
     <div class="container p-5 text-center">
         <h2>Blah bla bla</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia quam autem excepturi suscipit eum nisi,
@@ -117,3 +132,59 @@
         <button>Read More</button>
     </div>
 </div>
+
+<div class="container my-5">
+<div class="row">
+    <div class="col-md-6 mt-md-5">
+        <img src="{{asset('frontend/assets/images/about/img3.jpeg')}}" alt="" class="img-fluid rounded">
+    </div>
+    <div class="col-md-6 my-auto">
+        <h2 class="fs-32 text-capitalize">Unlock Creativity and endless adventures</h2>
+        <div class="divider"></div>
+        <p>Our team at <span class="r-text-red">RANGO CART</span> is made up of passionate individuals who are dedicated to creating the best toys for children. We have designers, engineers, and play experts who work together to ensure that our products are both fun and educational.</p>
+        <a href="" class="fs-13 fw-bold underline">Explore Our Toys <img src="{{asset('frontend/assets/images/about/arrow.svg')}}" alt=""></a>
+
+    </div>
+</div>
+</div>
+
+<script>
+    let hero_tl = gsap.timeline({
+    });
+    hero_tl.from('.hero-section .top h2 span',{
+        scale:0,
+        ease: "bounce.out",
+        duration:1,
+        stagger:0.2
+    },'anim0')
+    hero_tl.from('.hero-section .bottom h2 span',{
+        scale:0,
+        ease: "bounce.out",
+        duration:1,
+        stagger:0.2
+    },'anim0')
+    hero_tl.to('.hero-section .top',{
+        top:'-100%',
+        ease: "slow(0.7,0.7,false)",
+        duration:1,
+        delay:1.5,
+    },"anim1")
+    hero_tl.to('.hero-section .bottom',{
+        top:'150%',
+        ease: "slow(0.7,0.7,false)",
+        duration:1,
+        delay:1.5,
+    },"anim1")
+    hero_tl.from('.center .content *',{
+        x:-50,
+        opacity:0,
+        ease: "slow(0.7,0.7,false)",
+        stagger:0.1
+    },'anim_center')
+    hero_tl.from('.center img',{
+        x:-50,
+        scale:0,
+        opacity:0,
+        ease: "slow(0.7,0.7,false)",
+    },'anim_center')
+</script>
