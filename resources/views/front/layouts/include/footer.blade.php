18b923976d9984b0dbe54382f6eb6014275e8519
talha98ansari <talha98ansari@gmail.com>
2024-09-14 04:04:12 +0500

    changes

--- resources/views/front/layouts/include/footer.blade.php
+++ resources/views/front/layouts/include/footer.blade.php
@@ -26,6 +26,9 @@
                         </ul>
                     </li>
                     <li class="nav-item">
+                        <a class="" href="{{route('onSale')}}">Sale</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="" href="{{route('about.us')}}">About</a>
                     </li>
                     <li class="nav-item">
