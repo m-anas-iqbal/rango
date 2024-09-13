62634b1a0b798d4ad19d5048682c6e5db0f4935d
talha98ansari <talha98ansari@gmail.com>
2024-09-13 21:32:10 +0500

    changes

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -18,7 +18,7 @@ class SendMail extends Mailable
      */
     public function __construct($dt)
     {
-        $this->subject = 'Reply from Zairito';
+        $this->subject = 'Reply from Rango Cart';
         $this->dt = $dt;
     }
 
