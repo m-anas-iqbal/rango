56ea629599bc6c5a4f67acaf1a9b2993ddbe555f
talha98ansari <talha98ansari@gmail.com>
2024-09-15 01:42:06 +0500

    chnges

--- resources/views/components/frontend/counter.blade.php
+++ resources/views/components/frontend/counter.blade.php
@@ -94,12 +94,13 @@ $specialoffer = SpecialOffer();
                 <div class="wrap">
                 <h4>{{ $specialoffer->en_title }}</h4>
                 <p{{ $specialoffer->description }}</p>
-                // <a href="{{ $specialoffer->url }}" class="btn btn-outline-light">Shop Now</a>
                 </div>
                 `;
                 msg_counter.classlist.add('d-none');
             }
         }, 1000);
+                        // <a href="{{ $specialoffer->url }}" class="btn btn-outline-light">Shop Now</a>
+
     </script>
 @endif
 @endsection
